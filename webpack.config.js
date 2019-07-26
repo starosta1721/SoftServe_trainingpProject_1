03cc6b90106415f043aa1cfaafe76377dfd18574
starosta1721 <starosta1721@gmail.com>
2019-07-26 23:07:05 +0300

    new configuration

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+    vendor: './src/vendor.ts',
+    style: './src/index.css',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+};
